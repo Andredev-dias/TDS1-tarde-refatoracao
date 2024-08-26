572003381f27ff13a15e122ce255eac14fbf2e33
Andredev-dias <andredias.curitiba@gmail.com>
2024-08-26 15:15:25 -0300

    reaproveitamento de parametros para funcao global de calculo

---
+++ atv01/script.js
@@ -0,0 +1,22 @@
+function soma(){
+    var numero01 = document.getElementById("number1").valueAsNumber
+    var numero02 = parseFloat(document.getElementById("number2").value)
+    var resposta = document.getElementById("respSoma")
+    resposta.textContent = numero01 + numero02
+}
+
+function sub(){
+    var numero01 = parseFloat(document.getElementById("number3").value)
+    var numero02 = parseFloat(document.getElementById("number4").value)
+    var resposta = document.getElementById("respSub")
+    resposta.textContent = numero01 - numero02
+}
+
+function mult(){
+    var numero01 = parseFloat(document.getElementById("number5").value)
+    var numero02 = parseFloat(document.getElementById("number6").value)
+    var resposta = document.getElementById("respMult")
+    resposta.textContent = numero01 * numero02
+}
+
+
